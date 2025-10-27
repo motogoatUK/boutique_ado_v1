e8f2a9990c0d4221eb8ddb7bc2a6ecb0ecd63b4a
motogoatUK <software@motogoat.co.uk>
2025-10-27 20:27:16 +0000

    Added toast messages to bag

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -11,6 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -33,6 +36,7 @@ def add_to_bag(request, item_id):
         else:
             bag[item_id] = quantity
 
+    messages.success(request, f'Added {product.name} to your bag')
     request.session['bag'] = bag
     return redirect(redirect_url)
 
@@ -64,7 +68,7 @@ def adjust_bag(request, item_id):
 
 
 def remove_from_bag(request, item_id):
-    """ Adjust the quantity of the specified product in the shopping bag """
+    """ Remove the specified product from the shopping bag """
     try:
         size = None
         if 'product_size' in request.POST:
@@ -76,7 +80,7 @@ def remove_from_bag(request, item_id):
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
         else:
-            bag.pop[item_id]
+            bag.pop(item_id)
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
