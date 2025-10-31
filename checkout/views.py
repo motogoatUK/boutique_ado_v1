e2b9af1eda88f93c1a681ea56cf9ca0bb9ab4ea1
motogoatUK <software@motogoat.co.uk>
2025-10-31 01:51:21 +0000

    add Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,10 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SO5rIPaSLx1qvfSfTJR4'
+        'NRrTZRJ0WcNbBfpqHfLGAcs6S61TQCgs4y2G1xJuRcHLJdnklRTH'
+        'ijentedUCrgWkel00Sv5kvgIt',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
