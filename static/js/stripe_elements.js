a8314bad6993008a2976ab61a644c55b9f6de1e5
motogoatUK <software@motogoat.co.uk>
2025-11-01 15:58:51 +0000

    add basic checkout functionality

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -1,6 +1,6 @@
-const stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-const client_secret = $('#id_client_secret').text().slice(1, -1);
-const stripe = Stripe(stripe_public_key);
+const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+const clientSecret = $('#id_client_secret').text().slice(1, -1);
+const stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 var style = {
     base: {
@@ -19,3 +19,67 @@ var style = {
 };
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    const errorDiv =  document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+
+const form = document.getElementById('payment-form');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  card.update({ 'disabled': true});
+  $('submit-button').attr('disabled', true);
+  stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+//   const {error} = await stripe.confirmCardPayment({
+//     //`Elements` instance that was used to create the Payment Element
+//     card,
+//     elements,
+//     confirmParams: {
+//       return_url: 'https://example.com/order/123/complete',
+//     },
+//   });
+        if (result.error) {
+//   if (error) {
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Show error to your customer (for example, payment
+    // details incomplete)
+    const messageContainer = document.querySelector('#card-errors');
+    //messageContainer.textContent = error.message;
+    let html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span>${error.message}</span>
+        `;
+        messageContainer.innerHTML = html;
+        card.update({ 'disabled': false});
+        $('submit-button').attr('disabled', false);
+  } else {
+    // Your customer will be redirected to your `return_url`. For some payment
+    // methods like iDEAL, your customer will be redirected to an intermediate
+    // site first to authorize the payment, then redirected to the `return_url`.
+    if (result.paymentIntent.status === 'succeeded') {
+    form.submit();
+    }
+  }
+  });
+});
