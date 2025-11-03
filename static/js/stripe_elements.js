7ab66ee49ee289308d9ea1ca53969874de17a7ea
motogoatUK <software@motogoat.co.uk>
2025-11-03 16:07:48 +0000

    added checkout formdata caching in payment intent

--- static/js/stripe_elements.js
+++ static/js/stripe_elements.js
@@ -17,12 +17,12 @@ var style = {
         iconColor: '#dc3545'
     }
 };
-let card = elements.create('card', {style: style});
+let card = elements.create('card', { style: style });
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
-    const errorDiv =  document.getElementById('card-errors');
+    const errorDiv = document.getElementById('card-errors');
     if (event.error) {
         let html = `
         <span class="icon" role="alert">
@@ -41,49 +41,89 @@ card.addEventListener('change', function (event) {
 const form = document.getElementById('payment-form');
 
 form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  card.update({ 'disabled': true});
-  $('submit-button').attr('disabled', true);
-  $('#payment-form').fadeToggle(100);
-  $('#loading-overlay').fadeToggle(100);
-  stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-//   const {error} = await stripe.confirmCardPayment({
-//     //`Elements` instance that was used to create the Payment Element
-//     card,
-//     elements,
-//     confirmParams: {
-//       return_url: 'https://example.com/order/123/complete',
-//     },
-//   });
-        if (result.error) {
-//   if (error) {
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Show error to your customer (for example, payment
-    // details incomplete)
-    const messageContainer = document.querySelector('#card-errors');
-    //messageContainer.textContent = error.message;
-    let html = `
-        <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
-        </span>
-        <span>${result.error.message}</span>
-        `;
-        messageContainer.innerHTML = html;
-        $('#payment-form').fadeToggle(100);
-        $('#loading-overlay').fadeToggle(100);
-        card.update({ 'disabled': false});
-        $('submit-button').attr('disabled', false);
-  } else {
-    // Your customer will be redirected to your `return_url`. For some payment
-    // methods like iDEAL, your customer will be redirected to an intermediate
-    // site first to authorize the payment, then redirected to the `return_url`.
-    if (result.paymentIntent.status === 'succeeded') {
-    form.submit();
-    }
-  }
-  });
+    event.preventDefault();
+    card.update({ 'disabled': true });
+    $('submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
+    let saveInfo = Boolean($("#id-save-info").attr('checked'));
+    // From using {% csrf_token %} in the form
+    let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    let postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    let url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
+        }).then(function (result) {
+            //   const {error} = await stripe.confirmCardPayment({
+            //     //`Elements` instance that was used to create the Payment Element
+            //     card,
+            //     elements,
+            //     confirmParams: {
+            //       return_url: 'https://example.com/order/123/complete',
+            //     },
+            //   });
+            if (result.error) {
+                //   if (error) {
+                // This point will only be reached if there is an immediate error when
+                // confirming the payment. Show error to your customer (for example, payment
+                // details incomplete)
+                const messageContainer = document.querySelector('#card-errors');
+                //messageContainer.textContent = error.message;
+                let html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>
+                `;
+                messageContainer.innerHTML = html;
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false });
+                $('submit-button').attr('disabled', false);
+            } else {
+                // Your customer will be redirected to your `return_url`. For some payment
+                // methods like iDEAL, your customer will be redirected to an intermediate
+                // site first to authorize the payment, then redirected to the `return_url`.
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        })
+    }).fail(function(){
+        // just reload the page, the error will be in django messages
+        location.reload();
+    })
 });
