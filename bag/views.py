f76496be3faee85b77f6f99d5f35b88c57387d5f
motogoatUK <software@motogoat.co.uk>
2025-11-03 15:50:40 +0000

    fix: bag preview and delivery calculation

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import \
-    render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 from products.models import Product
 
@@ -28,16 +28,19 @@ def add_to_bag(request, item_id):
                 bag[item_id]['items_by_size'][size] += quantity
                 messages.success(
                     request,
-                    f'Updated size {size.upper()}{product.name}'
-                    ' quantity to {bag[item_id]["items_by_size"][size]}')
+                    (f'Updated size {size.upper()} {product.name}'
+                     f' quantity to {bag[item_id]["items_by_size"][size]}')
+                    )
             else:
                 bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()}'
-                                 ' {product.name} to your bag')
+                messages.success(
+                    request,
+                    f'Added size {size.upper()} {product.name} to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()}'
-                             ' {product.name} to your bag')
+            messages.success(
+                request,
+                f'Added size {size.upper()} {product.name} to your bag')
 
     else:
         if item_id in list(bag.keys()):
@@ -66,8 +69,10 @@ def adjust_bag(request, item_id):
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
             messages.success(
-                request, f'Updated size {size.upper()} {product.name} quantity'
-                ' to {bag[item_id]["items_by_size"][size]}')
+                request,
+                (f'Updated size {size.upper()} {product.name} quantity'
+                 f' to {bag[item_id]["items_by_size"][size]}')
+            )
 
         else:
             del bag[item_id]['items_by_size'][size]
