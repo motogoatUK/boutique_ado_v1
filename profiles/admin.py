7ae2c182900fbc8d1d60e376ad638a1d5b7d9f54
motogoatUK <software@motogoat.co.uk>
2025-11-05 15:59:33 +0000

    add: user profile to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import UserProfile
+
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'default_phone_number',
+                    'default_street_address1', 'default_street_address2',
+                    'default_town_or_city', 'default_postcode',
+                    'default_country')
+
+
+admin.site.register(UserProfile, ProfileAdmin)
