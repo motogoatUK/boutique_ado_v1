28ffdb89808ea2fff6e8bcaf14d5f2d01d400fb9
motogoatUK <software@motogoat.co.uk>
2025-11-07 15:38:09 +0000

    fix the image field in product admin

--- products/views.py
+++ products/views.py
@@ -78,7 +78,7 @@ def add_product(request):
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
-    
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -106,7 +106,7 @@ def edit_product(request, product_id):
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
-    
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
